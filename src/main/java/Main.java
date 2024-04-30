5312e99f5287425678c8a4f70712bf9cf8a56d46
Iván Villagrasa <ivvil412@gmail.com>
2024-04-30 20:59:08 +0200

    Add hello world

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+
+public class Main extends SimpleApplication {
+    public static void main(String[] args){
+        Main app = new Main();
+        app.start(); // start the game
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+                "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+    }
+
+}
