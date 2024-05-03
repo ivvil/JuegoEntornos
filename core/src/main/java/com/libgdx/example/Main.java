6856c386272043089afab785719585d8269ab42a
Iván Villagrasa <ivvil412@gmail.com>
2024-05-03 13:50:43 +0200

    Initial commit

---
+++ core/src/main/java/com/libgdx/example/Main.java
@@ -0,0 +1,105 @@
+package com.libgdx.example;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.awt.*;
+import java.util.Iterator;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+//    private Texture image;
+    private Texture dropImage;
+    private Texture bucketImage;
+    private OrthographicCamera camera;
+    private Rectangle bucket;
+    private Array<Rectangle> raindrops;
+    private long lastDropTime;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+//        image = new Texture("libgdx.png");
+        dropImage = new Texture(Gdx.files.internal("drop.png"));
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 400);
+        batch = new SpriteBatch();
+
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
+
+        raindrops = new Array<>();
+        spawnRaindrop();
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        batch.draw(bucketImage, bucket.x, bucket.y);
+        batch.end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+        if (bucket.x < 0) bucket.x = 0;
+        if (bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+        for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (raindrop.y + 64 < 0) iter.remove();
+            if (raindrop.overlaps(bucket)) {
+                iter.remove();
+            }
+        }
+
+//        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        batch.begin();
+//        batch.draw(image, 140, 210);
+//        batch.end();
+    }
+
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800 - 64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+    @Override
+    public void dispose() {
+        dropImage.dispose();
+        bucketImage.dispose();
+        batch.dispose();
+//        image.dispose();
+    }
+}
