fb6b360dc48f2ebd05fd830e9f44349d6638170b
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-05-06 10:02:48 +0200

    add point2

---
+++ JuegoCliente/src/main/java/org/example/Datatypes/Point2.java
@@ -0,0 +1,17 @@
+package org.example.Datatypes;
+
+public class Point2 {
+	public double x;
+	public double y;
+	
+	public Point2(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Point2() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+}
