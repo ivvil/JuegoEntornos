4cc1d844328f0afe6fea6cbce0c92bb7fa1111f6
mariol03 <mario.laglerabarco@gmail.com>
2024-05-06 11:05:43 +0200

    Add HighScore Spring Application

---
+++ JuegoHighScore/src/main/java/com/example/juegohighscore/JuegoHighScoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.juegohighscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoHighScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuegoHighScoreApplication.class, args);
+    }
+
+}
---
+++ JuegoHighScore/src/test/java/com/example/juegohighscore/JuegoHighScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juegohighscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoHighScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
